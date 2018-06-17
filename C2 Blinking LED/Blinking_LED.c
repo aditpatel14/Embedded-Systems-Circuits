7a58f8da0e3b1b92ba98c45f461c35ee0729ce07
aditpatel14 <aditpatel14@yahoo.ca>
2018-06-16 23:47:09 -0400

    Timer Interrupt used for blinking led

--- C2 Blinking LED/Blinking_LED.c
+++ C2 Blinking LED/Blinking_LED.c
@@ -1,8 +1,8 @@
 /*
- * MCU: ATtiny13
- * Keep one LED turned on while the other flashes using delays
- * Created by TronClub.com
- */
+* MCU: ATtiny13
+* Keep one LED turned on while the other flashes using delays
+* Created by TronClub.com
+*/
 
 #include <avr/io.h>
 #include <util/delay.h> 						// Allows the use of delays
@@ -15,6 +15,6 @@ int main(void) {
 		_delay_ms(500);
 		PORTB = (0<<PB4) | (1<<PB3) ;							//Set only PB4 as high, PB3 is low
 		_delay_ms(500);
-		}
+	}
 	return 0;
 }
